a4a19ddf1b966c3c31b9cc12b06d8a64f33f3a2d
VY <kodiers@gmail.com>
2017-08-10 02:01:01 +0300

    Added user registration api view

--- people/api/urls.py
+++ people/api/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
 
+from .views import UserRegistrationAPIView
+
 
 urlpatterns = (
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-verify/', verify_jwt_token),
     url(r'^api-token-refresh/', refresh_jwt_token),
+    url(r'^register/$', UserRegistrationAPIView.as_view(), name='registration')
 )
