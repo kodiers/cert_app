09a23b39bb02a5f4a2c15fad78254b72e6565eae
Victor Yamchinov <kodiers@gmail.com>
2019-04-26 04:10:47 +0300

    restore previous db structure

--- cert_app/api_urls.py
+++ cert_app/api_urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url, include
 app_name = 'api'
 
 urlpatterns = (
-    url(r'^people/', include('people.urls', namespace='people_api')),
+    url(r'^people/', include('people.api.urls', namespace='people_api')),
     url(r'^certifications/', include('certifications.api.urls', namespace='certifications_api')),
     url(r'^remainder/', include('cert_remainder.api.urls', namespace='cert_remainder_api'))
 )
