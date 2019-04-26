b74e8b0bc0d3110c559fb002b369ac9747595c0f
Victor Yamchinov <kodiers@gmail.com>
2019-04-26 04:18:07 +0300

    changed urls to path for api_urls

--- cert_app/api_urls.py
+++ cert_app/api_urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url, include
+from django.urls import path, include
 
 
 app_name = 'api'
 
 urlpatterns = (
-    url(r'^people/', include('people.api.urls', namespace='people_api')),
-    url(r'^certifications/', include('certifications.api.urls', namespace='certifications_api')),
-    url(r'^remainder/', include('cert_remainder.api.urls', namespace='cert_remainder_api'))
+    path('people/', include('people.api.urls', namespace='people_api')),
+    path('certifications/', include('certifications.api.urls', namespace='certifications_api')),
+    path('remainder/', include('cert_remainder.api.urls', namespace='cert_remainder_api'))
 )
