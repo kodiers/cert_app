40099f660c2bab155c8e38921ec35674233a2286
Victor Yamchinov <kodiers@gmail.com>
2019-05-04 03:30:19 +0300

    certification admin customization

--- certifications/admin.py
+++ certifications/admin.py
@@ -5,6 +5,18 @@ from .models import Vendor, Certification, Exam
 # Register your models here.
 
 
+class CertificationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'number', 'vendor', 'deprecated')
+    search_fields = ('title', 'number')
+    list_filter = ('vendor', 'deprecated')
+
+
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('title', 'number', 'deprecated')
+    search_fields = ('title', 'number', 'certification__title')
+    list_filter = ('deprecated', 'certification__vendor')
+
+
 admin.site.register(Vendor)
-admin.site.register(Certification)
-admin.site.register(Exam)
+admin.site.register(Certification, CertificationAdmin)
+admin.site.register(Exam, ExamAdmin)
