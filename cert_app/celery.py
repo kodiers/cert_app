2030293e52a84e53cbc8bcdb59c3568b7f747566
Victor Yamchinov <kodiers@gmail.com>
2019-05-09 04:55:44 +0300

    fix celery configuration added scripts to run workers

--- cert_app/celery.py
+++ cert_app/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cert_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
 app = Celery('cert_app')
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
---
+++ start_celery.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Commands to run celery workers and beat
+
+celery -A cert_app worker -l debug -n worker.high -Q high &
+celery -A cert_app worker -l debug -n worker.normal -Q normal &
+celery -A cert_app worker -l debug -n worker.low -Q low &
+celery beat -A cert_app &
