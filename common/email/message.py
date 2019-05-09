4046103433af5e3f78c6ba087888d8d4b3a44584
Victor Yamchinov <kodiers@gmail.com>
2019-05-09 04:56:16 +0300

    added email wrapper

---
+++ common/email/message.py
@@ -0,0 +1,21 @@
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+
+
+class Email:
+    """
+    Email class as wrapper to standard Django EmailMultiAlternatives class
+    """
+    def __init__(self, context: dict, template_name: str, subject: str, receivers: list):
+        html_content = render_to_string(template_name, context)
+        text_content = strip_tags(html_content)
+        self.message = EmailMultiAlternatives(subject, text_content, settings.EMAIL_HOST_USER, receivers)
+        self.message.attach_alternative(html_content, 'text/html')
+
+    def send(self):
+        """
+        Send email message
+        """
+        self.message.send()
