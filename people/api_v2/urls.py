ff6abc4e2f647cb3a36c8b9760e86ccf2dc459b6
Victor Yamchinov <kodiers@gmail.com>
2019-05-24 03:54:38 +0300

    added password reset url

--- people/api_v2/urls.py
+++ people/api_v2/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import UserRegistrationAPIViewV2
+from .views import UserRegistrationAPIViewV2, RequestPasswordResetTokenAPIViewV2
 
 
 app_name = 'people_api_v2'
 
 urlpatterns = (
     path('register/', UserRegistrationAPIViewV2.as_view(), name='registration'),
+    path('password/reset/', RequestPasswordResetTokenAPIViewV2.as_view(), name='password_reset')
 )
