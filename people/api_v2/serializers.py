1e7ebf8a004988a56fbd746d756181333d072738
Victor Yamchinov <kodiers@gmail.com>
2019-05-28 04:49:37 +0300

    added password reset view, serializer and email task

--- people/api_v2/serializers.py
+++ people/api_v2/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 from people.models import PasswordResetToken
 from people.api.serializers import UserRegistrationSerializer
-from people.api_v2.validators import EmailValidator
+from people.api_v2.validators import EmailValidator, PasswordValidator
 
 
 class UserRegistrationSerializerV2(UserRegistrationSerializer):
@@ -44,3 +44,35 @@ class RequestPasswordResetSerializer(serializers.Serializer):
         email = validated_data.get('email')
         instance = PasswordResetToken.create_password_reset_token(email)
         return instance
+
+
+class ResetPasswordSerializer(serializers.Serializer):
+    """
+    Password reset serializer
+    """
+    token = serializers.CharField(max_length=64)
+    password = serializers.CharField(min_length=8, max_length=50, validators=[PasswordValidator()])
+    confirm_password = serializers.CharField(min_length=8, max_length=50)
+
+    def validate_token(self, value):
+        try:
+            token = PasswordResetToken.objects.get(token=value)
+        except PasswordResetToken.DoesNotExist:
+            raise serializers.ValidationError("Token incorrect.")
+        if token.expired:
+            raise serializers.ValidationError("Token is expired.")
+        return value
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        confirm_password = attrs.get('confirm_password')
+        token = attrs.get('token')
+        if password != confirm_password:
+            raise serializers.ValidationError(_("Password and confirm password don't match"))
+        try:
+            user = User.objects.get(reset_token__token=token)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("Token incorrect.")
+        if not user.is_active:
+            raise serializers.ValidationError("User is not active.")
+        return attrs
--- people/api_v2/views.py
+++ people/api_v2/views.py
@@ -7,11 +7,11 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.parsers import JSONParser
 
-from people.models import Profile
-from people.tasks import send_registration_confirmation, send_password_reset_email
+from people.models import Profile, PasswordResetToken
+from people.tasks import send_registration_confirmation, send_password_reset_email, send_password_reset_success
 from people.api.serializers import ProfileSerializer
 
-from .serializers import UserRegistrationSerializerV2, RequestPasswordResetSerializer
+from .serializers import UserRegistrationSerializerV2, RequestPasswordResetSerializer, ResetPasswordSerializer
 
 
 class UserRegistrationAPIViewV2(GenericAPIView):
@@ -47,3 +47,23 @@ class RequestPasswordResetTokenAPIViewV2(CreateAPIView):
         instance = serializer.save()
         send_password_reset_email.delay(instance.user.username, instance.user.email, instance.token)
         return Response({'result': "Ok", 'message': 'Password reset email was sent.'}, status=status.HTTP_201_CREATED)
+
+
+class ResetPasswordAPIViewV2(GenericAPIView):
+    """
+    Reset password using token
+    """
+    serializer_class = ResetPasswordSerializer
+    parser_classes = (JSONParser,)
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request: Request) -> Response:
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        token = PasswordResetToken.objects.get(token=serializer.validated_data['token'])
+        user = token.user
+        token.delete()
+        user.set_password(serializer.validated_data['password'])
+        user.save()
+        send_password_reset_success.delay(user.username, user.email)
+        return Response({'result': 'Ok', 'message': 'Your password was reseted.'}, status=status.HTTP_200_OK)
