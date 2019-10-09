7a1172bb1349348c4d035384ec9236a061504bd0
Victor Yamchinov <kodiers@gmail.com>
2019-10-09 03:05:24 +0300

    added parser config model

--- parsers/admin.py
+++ parsers/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import ParserConfig
+
 # Register your models here.
+
+
+class ParserConfigAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'url')
+    search_fields = ('vendor__title', 'url', 'parser_class_id')
+
+
+admin.site.register(ParserConfig, ParserConfigAdmin)
--- parsers/models.py
+++ parsers/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from common.models import BaseModel
+from certifications.models import Vendor
 
 # Create your models here.
+
+
+class ParserConfig(BaseModel):
+    """
+    Model for configuration for parsers
+    """
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, verbose_name=_('Vendor'))
+    url = models.URLField(verbose_name=_('URL'))
+    parser_class_id = models.CharField(verbose_name='Parser ID', max_length=30, unique=True)
+
+    def __str__(self):
+        return f'{self.vendor.title} - {self.parser_class_id}'
+
+    class Meta:
+        verbose_name = _('Parser config')
+        verbose_name_plural = _('Parser configs')
+        ordering = ('created',)
