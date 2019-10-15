7e8270568105f00774efb10fcbbc260774c92a6c
Victor Yamchinov <kodiers@gmail.com>
2019-10-16 02:42:14 +0300

    start working on parsers added dynamic http client

--- parsers/parsers/client.py
+++ parsers/parsers/client.py
@@ -1,8 +1,17 @@
 import requests
 from requests.adapters import HTTPAdapter
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 
-class ParserHttpClient:
+from django.conf import settings
+
+
+
+class StaticHttpClient:
     """
 
     """
@@ -10,7 +19,7 @@ class ParserHttpClient:
     timeout = 3
 
     def __init__(self):
-        self.session = requests.Session()
+        self.session = self.engine.Session()
         self.adapter = HTTPAdapter(max_retries=5)
         self.session.mount('https://', self.adapter)
         self.session.mount('http://', self.adapter)
@@ -24,3 +33,29 @@ class ParserHttpClient:
         """
         data = self.session.get(url, headers=headers)
         return data.text
+
+
+class DynamicHttpClient:
+    """
+
+    """
+    timeout_for_page_load = 10
+
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        self.driver = webdriver.Chrome(settings.CHROME_WEBDRIVER_PATH, options=chrome_options)
+
+    def get(self, url: str, element_class: str) -> str:
+        """
+
+        :param url:
+        :param element_class:
+        :return:
+        """
+        self.driver.get(url)
+        wait = WebDriverWait(self.driver, self.timeout_for_page_load)
+        wait.until(expected_conditions.visibility_of_all_elements_located((By.CLASS_NAME, element_class)))
+        data = self.driver.page_source
+        self.driver.close()
+        return data
